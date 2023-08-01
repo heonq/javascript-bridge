c5bf3cf6248647410df6a01b6cf0d84a70bc61cc
heonq <gkagjsrb@naver.com>
2023-08-01 18:06:46 +0900

    feat: 다리 길이 입력받는 기능 구현

--- src/Controller.js
+++ src/Controller.js
@@ -1,9 +1,13 @@
 const OutputView = require('./OutputView');
+const InputView = require('./InputView');
 
 class Controller {
   play() {
     OutputView.printIntro();
+    InputView.readBridgeSize(this.handleBridgeSize.bind(this));
   }
+
+  handleBridgeSize(size) {}
 }
 
 module.exports = Controller;
