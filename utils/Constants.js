aa92e24fabaf9757feb031a572fee9bcba0b76f6
heonq <gkagjsrb@naver.com>
2023-08-01 17:54:56 +0900

    feat: 게임 진행을 위한 메시지,상수 파일 추가

---
+++ utils/Constants.js
@@ -0,0 +1,18 @@
+const CONSTANTS = Object.freeze({
+  bridgeSizeMin: 3,
+  bridgeSizeMax: 20,
+  up: 'U',
+  down: 'D',
+  retry: 'R',
+  quit: 'Q',
+  success: '성공',
+  failure: '실패',
+  passable: 'O',
+  impassable: 'X',
+  blank: ' ',
+  division: ' | ',
+  bridgeStart: '[ ',
+  bridgeEnd: ' ]',
+});
+
+module.exports = CONSTANTS;
---
+++ utils/Message.js
@@ -0,0 +1,11 @@
+const MESSAGE = Object.freeze({
+  gameStart: '다리 건너기 게임을 시작합니다.\n',
+  askBridgeSize: '다리의 길이를 입력해주세요.\n',
+  askDirection: '이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+  askRetry: '게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+  gameResult: '최종 게임 결과\n',
+  successOrNot: '게임 성공 여부: ',
+  trialCount: '총 시도한 횟수: ',
+});
+
+module.exports = MESSAGE;
