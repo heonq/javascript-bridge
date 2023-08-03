efc25ce415f4157e74a119a93e7135150d604e79
heonq <gkagjsrb@naver.com>
2023-08-03 21:34:49 +0900

    test: 끝까지 건너가는데 성공했는지 확인하는 기능 테스트 구현

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -9,4 +9,12 @@ describe('BridgeGame 단위 테스트', () => {
     direction.forEach((a) => bridgeGame.move(a, bridge));
     expect(bridgeGame.checkBlocked(direction[2])).toEqual(result);
   });
+  test.each([
+    [['U', 'D', 'D'], ['U', 'D', 'D'], true],
+    [['U', 'D', 'D', 'U'], ['U', 'D', 'D'], false],
+  ])('끝까지 건너가는데 성공했는지 확인하는 기능 테스트', (bridge, directions, result) => {
+    const bridgeGame = new BridgeGame();
+    directions.forEach((direction) => bridgeGame.move(direction, bridge));
+    expect(bridgeGame.checkSuccess(bridge.length)).toEqual(result);
+  });
 });
