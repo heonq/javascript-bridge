ba16136635f0a4791c9566869de89f4418b3cf00
heonq <gkagjsrb@naver.com>
2023-08-04 13:00:54 +0900

    fix: 사용자가 입력한 값에 대한 유효성 검사 중 대소문자 구분 제거

--- utils/Validator.js
+++ utils/Validator.js
@@ -9,19 +9,12 @@ const Validator = {
     return true;
   },
   validateDirection(direction) {
-    if (direction !== direction.toUpperCase()) {
-      return handleError(ERROR_MESSAGE.capital);
-    }
     if (direction !== 'U' && direction !== 'D') {
       return handleError(ERROR_MESSAGE.direction);
     }
     return true;
   },
   validateCommand(command) {
-    if (command !== command.toUpperCase()) {
-      return handleError(ERROR_MESSAGE.capital);
-    }
-
     if (command !== 'R' && command !== 'Q') {
       return handleError(ERROR_MESSAGE.command);
     }
